271c5ec5c2e6853884276eec23dcd01c98d235e8
Gofile <yantonuri917@gmail.com>
2026-06-29 02:18:05 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    // URL Supabase Anda (Bisa dilihat di Project Settings -> API)
+    SUPABASE_URL: 'https://ehhrvswzhirrnrqxmxae.supabase.co',
+    
+    // Anon/Public API Key (Bisa dilihat di Project Settings -> API)
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoaHJ2c3d6aGlycm5ycXhteGFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMjg2NDcsImV4cCI6MjA5NTgwNDY0N30.75vZs3Rn6j7YSlInHpGDN39OGjhywcEBv8j-hawyOlY'
+};
